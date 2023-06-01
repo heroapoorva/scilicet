bff99af2099204213a119f88a8859f7a7af5b055
Apoorva Tamaskar <heroapoorva@gmail.com>
2023-06-01 19:16:59 -0400

    better readme

--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.jdbc.core.JdbcTemplate;
-import com.example.springboot.Student;
 
 @RestController
 public class HelloController {
